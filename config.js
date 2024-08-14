a3ecae01693a2b773dbce9ab03c75bd010e042d0
Erika Hentsch <erika.hentsch@rossvideo.com>
2024-08-14 13:30:50 -0400

    first commit

---
+++ config.js
@@ -0,0 +1,21 @@
+var CONFIG = {
+    "august_2024": {
+        files: [
+            {
+                electionEvent: 'Primaries', 
+                fileName: 'Florida_primary.json', 
+                entry: 'ElectionPlaylist'
+            }, 
+            {
+                electionEvent: 'Local',
+                fileName: 'Florida_local.json', 
+                entry: 'ElectionEvent'
+            }, 
+            // {
+            //     electionEvent: 'General',
+            //     fileName: 'Florida_dunno.json', 
+            //     entry: 'ElectionEvent'
+            // }
+        ]
+    }
+}
